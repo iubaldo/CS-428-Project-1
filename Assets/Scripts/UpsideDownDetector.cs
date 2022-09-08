2b32294d0b374687f83e4b44bb8891f4d711ae21
iubaldo <ianzachary.u@gmail.com>
2022-09-08 16:46:45 -0500

    implemented lights and light switching on upside down

--- Assets/Scripts/UpsideDownDetector.cs
+++ Assets/Scripts/UpsideDownDetector.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class UpsideDownDetector : MonoBehaviour
 {
-    public GameObject knickknack;
+    public bool useNormalLighting = true;
+    public bool canChangeLighting = false;
+    public GameObject normalLight;
+    public GameObject raveLight;
+    public GameObject sunlight;
 
-    // Update is called once per frame
+
+    private void Start()
+    {
+        InvokeRepeating("changeColor", 1f, 1f);
+    }
+
+
+    void changeColor()
+    {
+        raveLight.GetComponent<Light>().color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+
+
     void FixedUpdate()
     {
-        
+        if (!canChangeLighting && Vector3.Dot(transform.up, Vector3.down) > 0.75) // cube is upside down
+            canChangeLighting = true;
+
+
+        if (canChangeLighting && Vector3.Dot(transform.up, Vector3.up) > 0.75) // cube is right side up
+        {
+            canChangeLighting = false;                   
+            useNormalLighting = !useNormalLighting;
+
+            normalLight.GetComponent<Light>().enabled = useNormalLighting;
+            raveLight.GetComponent<Light>().enabled = !useNormalLighting;
+
+            if (useNormalLighting)
+                sunlight.GetComponent<Light>().intensity = 0.5f;
+            else
+                sunlight.GetComponent<Light>().intensity = 0f;
+        }
     }
 }
--- Assets/Scripts/beamsaberspin.cs
+++ Assets/Scripts/beamsaberspin.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class beamsaberspin : MonoBehaviour
 {
-    public GameObject target;
     public float spinSpeed;
 
     void FixedUpdate()
     {
-        target.transform.rotation *= Quaternion.Euler(Vector3.forward * spinSpeed * Time.deltaTime);
-        Debug.Log("changing rotation");
+        transform.rotation *= Quaternion.Euler(Vector3.forward * spinSpeed * Time.deltaTime);
     }
 }
